469b9eedccb2d9aa0ab5e367ea726ca1cd1fef43
Jasminems <jasmine.srakj@gmail.com>
2021-07-08 00:09:03 +0530

    Concatenate

---
+++ Conc/src/Conc.java
@@ -0,0 +1,9 @@
+/*A Java program to concatenate two Strings*/
+class Conc 
+{
+	public static void main(String[] args) 
+	{
+		String s="Java"+"Program";    //using '+' operator
+		System.out.println(s);
+	}
+}

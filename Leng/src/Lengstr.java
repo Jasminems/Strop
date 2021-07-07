31ebce659f45da398213289b37b43650e5082ec0
Jasminems <jasmine.srakj@gmail.com>
2021-07-08 00:10:55 +0530

    Length

---
+++ Leng/src/Lengstr.java
@@ -0,0 +1,9 @@
+/*A Java program to find the length of the String*/
+class Lengstr 
+{
+	public static void main(String[] args) 
+	{
+		String s="JavaProgram";
+		System.out.println("Length of the string is:"+s.length());
+	}
+}

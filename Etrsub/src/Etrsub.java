6f06f4207b4f8fbb22cc2b53fa5ba9536fbc1c8c
Jasminems <jasmine.srakj@gmail.com>
2021-07-08 00:12:31 +0530

    Extract a substring

---
+++ Etrsub/src/Etrsub.java
@@ -0,0 +1,11 @@
+/*A Java program to extract a String using Substring*/
+class Etrsub 
+{
+	public static void main(String[] args) 
+	{
+		String s="JavaStringProgram";
+		System.out.println(s);
+		System.out.println(s.substring(4));
+		System.out.println(s.substring(10));
+	}
+}
